6a033bd0612784e56aeb03a4a430912f7cd77daa
1526414 <1526414@uab.cat>
2023-11-05 01:19:21 +0100

    IT DOES THINGS NOW

--- Plugin.cs
+++ Plugin.cs
@@ -11,6 +11,7 @@ using HarmonyLib;
 using System.Linq;
 using BSG.CameraEffects;
 using System.Xml.Linq;
+using EFT.InventoryLogic;
 
 namespace BorkelRNVG
 {
@@ -37,14 +38,7 @@ namespace BorkelRNVG
                     Logger.LogMessage($"Texture2D 0: {masks[0].name}"); //mask 0: mask_anvis
                     Logger.LogMessage($"Texture2D 1: {masks[1].name}"); //mask 1: mask_binocular
                     Logger.LogMessage($"Texture2D 2: {masks[2].name}"); //mask 2: mask_old_monocular
-                    //new AccessNightVisiondPatch().Enable();
-                    /*Type NVType = typeof(NightVision);
-                    FieldInfo NVFieldInfo = NVType.GetField("Mask", BindingFlags.Public | BindingFlags.Instance);
-                    if (!Singleton<GameWorld>.Instantiated)
-                    {
-                        Logger.LogError("Failed to load NightVision instance.");
-                    }
-                        Logger.LogMessage($"Value of Mask is: {NVFieldInfo.GetValue(Singleton<NightVision>.Instance)}");*/
+                    new SetMaskPatch().Enable();
                 }
                 else
                 {
@@ -57,94 +51,34 @@ namespace BorkelRNVG
             }
         }
     }
-    public class AccessNightVisionController : MonoBehaviour
+    public class SetMaskPatch : ModulePatch
     {
-        private static NightVision NV; // field we'll use to hold the GameWorld instance
-        // mono method called every frame
-        void Update()
-        {
-            // check if game world is instantiated, instatiated = exists in scene
-            if (!Singleton<NightVision>.Instantiated) return; // don't allow logic to run if not instantiated
-
-            NV = Singleton<NightVision>.Instance;
-            //Logger.LogMessage($"Value of Mask is: {NVFieldInfo.GetValue(Singleton<NightVision>.Instance)}");
-            FieldInfo myMask = typeof(NightVision).GetField("Mask", BindingFlags.Public | BindingFlags.Instance);
-            FieldInfo NotMyAnvis = typeof(NightVision).GetField("AnvisMaskTexture", BindingFlags.Public | BindingFlags.Instance);
-            FieldInfo NotMyBino = typeof(NightVision).GetField("BinocularMaskTexture", BindingFlags.Public | BindingFlags.Instance);
-            FieldInfo NotMyMono = typeof(NightVision).GetField("OldMonocularMaskTexture", BindingFlags.Public | BindingFlags.Instance);
-            Texture2D mask = (Texture2D)myMask.GetValue(NV);
-            Texture2D anvis = (Texture2D)NotMyAnvis.GetValue(NV);
-            Texture2D bino = (Texture2D)NotMyBino.GetValue(NV);
-            Texture2D mono = (Texture2D)NotMyMono.GetValue(NV);
-            if (mask.name == anvis.name)
-            {
-                myMask.SetValue(NV, Plugin.masks[0]);
-            }
-            else if (mask.name == bino.name)
-            {
-                myMask.SetValue(NV, Plugin.masks[1]);
-            }
-            else if (mask.name == mono.name)
-            {
-                myMask.SetValue(NV, Plugin.masks[2]);
-            }
-            NotMyAnvis.SetValue(NV, Plugin.masks[0]);
-            NotMyBino.SetValue(NV, Plugin.masks[1]);
-            NotMyMono.SetValue(NV, Plugin.masks[2]);
-
-        }
-    }
-    public class AccessNightVisiondPatch : ModulePatch
-    {
-        // Inherit GetTargetMethod from ModulePatch
         protected override MethodBase GetTargetMethod()
-        {   // Get method from type, use BindingFlags.Public as it's a public method
-            return typeof(NightVision).GetMethod("SetMask", BindingFlags.Public | BindingFlags.Instance);
-        }
-        // Will be invoked after the "OnGameStarted" method is ran and the world is loaded
-        // from here you can access objects in the game world
-        [PatchPostfix]
-        void PostFix()
         {
-            // put logic here
-            /*FieldInfo myMask=typeof(NightVision).GetField("Mask", BindingFlags.Public | BindingFlags.Instance);
-            FieldInfo myAnvis=typeof(NightVision).GetField("AnvisMaskTexture", BindingFlags.Public | BindingFlags.Instance);
-            FieldInfo myBi=typeof(NightVision).GetField("BinocularMaskTexture", BindingFlags.Public | BindingFlags.Instance);
-            FieldInfo myMono=typeof(NightVision).GetField("OldMonocularMaskTexture", BindingFlags.Public | BindingFlags.Instance);*/
-            /*Logger.LogError("POSTFIX-BORKEL");
-            Logger.LogError($"Mask: {AccessNightVisionController.Update().Mask.name}");
-            if (AccessNightVisionController.Update().Mask.name == AccessNightVisionController.Update().AnvisMaskTexture.name)
-            {
-                AccessNightVisionController.Update().Mask = Plugin.masks[0];
-            }
-            else if (AccessNightVisionController.Update().Mask.name == AccessNightVisionController.Update().BinocularMaskTexture.name)
-            {
-                AccessNightVisionController.Update().Mask = Plugin.masks[1];
-            }
-            else if (AccessNightVisionController.Update().Mask.name == AccessNightVisionController.Update().OldMonocularMaskTexture.name)
-            {
-                AccessNightVisionController.Update().Mask = Plugin.masks[2];
-            }
-            AccessNightVisionController.Update().AnvisMaskTexture = Plugin.masks[0];
-            AccessNightVisionController.Update().BinocularMaskTexture = Plugin.masks[1];
-            AccessNightVisionController.Update().OldMonocularMaskTexture = Plugin.masks[2];*/
+            return typeof(NightVision).GetMethod("SetMask", BindingFlags.Instance | BindingFlags.Public);
         }
-    }
-}
-
-/*            if(__instance.Mask.name == __instance.AnvisMaskTexture.name)
+        [PatchPrefix]
+        private static bool Prefix(ref NightVision __instance)
+        {
+            //code goes here
+            if (__instance.Mask.name == __instance.AnvisMaskTexture.name)
             {
-                __instance.Mask = masks[0];
+                __instance.Mask = Plugin.masks[0];
             }
             else if (__instance.Mask.name == __instance.BinocularMaskTexture.name)
             {
-                __instance.Mask = masks[1];
+                __instance.Mask = Plugin.masks[1];
             }
             else if (__instance.Mask.name == __instance.OldMonocularMaskTexture.name)
             {
-                __instance.Mask = masks[2];
+                __instance.Mask = Plugin.masks[2];
             }
-            __instance.AnvisMaskTexture = masks[0];
-            __instance.BinocularMaskTexture = masks[1];
-            __instance.OldMonocularMaskTexture = masks[2];
-*/
+            __instance.AnvisMaskTexture = Plugin.masks[0];
+            __instance.BinocularMaskTexture = Plugin.masks[1];
+            __instance.OldMonocularMaskTexture = Plugin.masks[2];
+            return false; //prevents original method from running, so we can fully override it
+
+        }
+    }
+
+}
